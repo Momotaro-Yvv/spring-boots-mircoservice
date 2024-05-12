b04f4195a9c78abcb41fec534988a36fde894e10
Yvonne Tao <yvonne.tao@team.telstra.com>
2024-05-13 09:35:18 +1000

    fix type definition error by adding empty constructor to allow Jackson to perform deserialization

--- microservice/src/main/java/com/telstra/codechallenge/starredRepos/service/model/StarredRepoResponse.java
+++ microservice/src/main/java/com/telstra/codechallenge/starredRepos/service/model/StarredRepoResponse.java
@@ -11,6 +11,9 @@ import java.util.List;
 public class StarredRepoResponse {
     private List<StarredRepo> items;
 
+    public StarredRepoResponse() {
+    }
+
     public StarredRepoResponse(List<StarredRepo> items) {
         this.items = items;
     }
